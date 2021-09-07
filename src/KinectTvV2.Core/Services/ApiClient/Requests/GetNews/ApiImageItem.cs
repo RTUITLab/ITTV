f3f4da33e709e4077df4cb03c696bf7f56d9553f
Yury <66903760+bezlla@users.noreply.github.com>
2021-09-07 22:17:43 +0300

    Add ImageHelper

---
+++ src/KinectTvV2.Core/Services/ApiClient/Requests/GetNews/ApiImageItem.cs
@@ -0,0 +1,8 @@
+namespace KinectTvV2.Core.Services.ApiClient.Requests.GetNews
+{
+    public class ApiImageItem
+    {
+        public string Source { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
