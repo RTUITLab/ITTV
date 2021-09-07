3402e97eca746c68abfc65a315adba2d271cd9e2
Yury <66903760+bezlla@users.noreply.github.com>
2021-09-07 19:46:34 +0300

    Init struct

---
+++ KinectTvV2.Core/Services/ApiClient/MireaApiEndpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace KinectTvV2.Core.Services.ApiClient
+{
+    public static class MireaApiEndpoints
+    {
+        public const string BaseAddress = "https://www.mirea.ru";
+        public const string GetNewsEndpoint = "/news/";
+    }
+}
---
+++ KinectTvV2.Core/Services/ApiClient/Requests/GetNews/ApiNewsItem.cs
@@ -0,0 +1,16 @@
+﻿namespace KinectTvV2.Core.Services.ApiClient.Requests.GetNews
+{
+    public class ApiNewsItem
+    {
+        public ApiNewsItem()
+        { }
+
+        public ApiNewsItem(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
