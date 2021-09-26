c5f780855faafe50cbb8a6b2f117475c0bd9cc0f
Yury <66903760+bezlla@users.noreply.github.com>
2021-09-27 02:45:48 +0300

    Refactoring

---
+++ src/KinectTvV2.OldVersion/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Samples.Kinect.ControlsBasics.DataModel;
+using Microsoft.Samples.Kinect.ControlsBasics.TVSettings;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Microsoft.Samples.Kinect.ControlsBasics
+{
+    public static class Startup
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                // ...
+            }
+            else
+            {
+                try
+                {
+                    var app = new App();
+                    app.InitializeComponent();
+                    app.Run();
+                } catch (Exception e)
+                {
+                    try
+                    {
+                        File.AppendAllLines("./logs.txt", new[] { DateTime.Now.ToShortDateString() + "  " + DateTime.Now.ToLongTimeString() + "\t\t" + e});
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+
+                    if (!Settings.Instance.IsAdmin)
+                    {
+                        NewsUpdateThread.Instance.StopUpdating();
+                        Process.Start("ControlsBasics-WPF.exe");
+                    }
+                }
+            }
+        }
+    }
+}
