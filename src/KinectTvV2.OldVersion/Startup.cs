61bae6be1a9632335e2b9f9f1f8aa52e29885e07
Yury <66903760+bezlla@users.noreply.github.com>
2021-09-27 03:31:37 +0300

    Refactoring

--- src/KinectTvV2.OldVersion/Startup.cs
+++ src/KinectTvV2.OldVersion/Startup.cs
@@ -9,35 +9,22 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
     public static class Startup
     {
         [STAThread]
-        public static void Main(string[] args)
+        public static void Main()
         {
-            if (args != null && args.Length > 0)
+            try
             {
-                // ...
-            }
-            else
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            } 
+            catch (Exception e)
             {
-                try
+                File.AppendAllLines("./logs.txt", new[] { DateTime.Now.ToShortDateString() + "  " + DateTime.Now.ToLongTimeString() + "\t\t" + e});
+                
+                if (!Settings.Instance.IsAdmin)
                 {
-                    var app = new App();
-                    app.InitializeComponent();
-                    app.Run();
-                } catch (Exception e)
-                {
-                    try
-                    {
-                        File.AppendAllLines("./logs.txt", new[] { DateTime.Now.ToShortDateString() + "  " + DateTime.Now.ToLongTimeString() + "\t\t" + e});
-                    }
-                    catch (Exception)
-                    {
-                        // ignored
-                    }
-
-                    if (!Settings.Instance.IsAdmin)
-                    {
-                        NewsUpdateThread.Instance.StopUpdating();
-                        Process.Start("ControlsBasics-WPF.exe");
-                    }
+                    NewsUpdateThread.Instance.StopUpdating();
+                    Process.Start("ControlsBasics-WPF.exe");
                 }
             }
         }
