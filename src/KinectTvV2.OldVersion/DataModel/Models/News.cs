a32153a40e74f7822fae936961808b64edea80ce
Yury <66903760+bezlla@users.noreply.github.com>
2021-09-27 04:18:38 +0300

    Refactoring

--- src/KinectTvV2.OldVersion/DataModel/Models/News.cs
+++ src/KinectTvV2.OldVersion/DataModel/Models/News.cs
@@ -1,52 +1,56 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Windows.Media.Imaging;
-
 namespace Microsoft.Samples.Kinect.ControlsBasics.DataModel.Models
 {
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
-    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "File is from Windows Store template")]
     public class News
     {
-        private readonly List<byte[]> byteImageList;
+        private string title;
+        private string content;
+        private List<byte[]> byteImageList;
         private List<BitmapImage> imageList;
 
         public News(string title,string content, List<byte[]> images)
         {
-            Title = title;
-            Content = content;
+            this.title = title;
+            this.content = content;
             byteImageList = images;
         }
 
-        public string Content { get; }
-        public string Title { get; }
+        [JsonIgnore]
+        public BitmapImage Source => ConvertByteToImage(byteImageList[0]);
+
+        public string Content { get => content; }
+        public string Title { get => title; }
+        public List<byte[]> ByteImageList { get => byteImageList; set => byteImageList = value; }
 
         [JsonIgnore]
-        public List<BitmapImage> ImageList { get
-        {
-            return imageList ??= ConvertBytesToImages(byteImageList);
-        } }
+        public List<BitmapImage> ImageList { get { if (imageList == null) { imageList = ConvertBytesToImages(byteImageList); } return imageList; } }
 
 
         private BitmapImage ConvertByteToImage (byte[] array)
         {
-            using var ms = new MemoryStream(array);
-            
-            var image = new BitmapImage();
-            image.BeginInit();
-            image.CacheOption = BitmapCacheOption.OnLoad;
-            image.StreamSource = ms;
-            image.EndInit();
-            
-            return image;
+            using (var ms = new MemoryStream(array))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = ms;
+                image.EndInit();
+
+                return image;
+            }
         }
 
-        private List<BitmapImage> ConvertBytesToImages(IEnumerable<byte[]> arrays)
+        private List<BitmapImage> ConvertBytesToImages(List<byte[]> arrays)
         {
-            return arrays.Select(ConvertByteToImage).ToList();
+            List<BitmapImage> images = new List<BitmapImage>();
+            foreach (byte[] array in arrays)
+            {
+                images.Add(ConvertByteToImage(array));
+            }
+            return images;
         }
     }
 }
