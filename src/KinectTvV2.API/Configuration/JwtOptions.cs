49647c52150f19e918963fa8ff774f5f508db2db
Yury <66903760+bezlla@users.noreply.github.com>
2021-10-01 02:18:19 +0300

    Добавлена поддержка IdentityServer

---
+++ src/KinectTvV2.API/Configuration/JwtOptions.cs
@@ -0,0 +1,17 @@
+namespace KinectTvV2.API.Configuration
+{
+    /// <summary>
+    /// Options to configure JWT
+    /// </summary>
+    public class JwtOptions
+    {
+        /// <summary>
+        /// Authority to use when making OpenIdConnect calls.
+        /// </summary>
+        public string Authority { get; set; }
+        /// <summary>
+        /// Single valid audience value for any received OpenIdConnect token
+        /// </summary>
+        public string Audience { get; set; }
+    }
+}
