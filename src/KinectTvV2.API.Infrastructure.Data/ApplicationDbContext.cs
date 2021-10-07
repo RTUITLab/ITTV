2493543897c8c57e77f2890c48ddb17289054baa
Yury <66903760+bezlla@users.noreply.github.com>
2021-10-07 22:20:29 +0300

    Добавлена базовая конфигурация Бд

--- src/KinectTvV2.API.Infrastructure.Data/ApplicationDbContext.cs
+++ src/KinectTvV2.API.Infrastructure.Data/ApplicationDbContext.cs
@@ -1,7 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace KinectTvV2.API.Infrastructure.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext() : base()
+        { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("User ID=postgres;Password=root;Server=localhost;Port=5432;Database=ittv-backend;Integrated Security=true;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
