2fdd177ed83f702b33a13080c2912b8d608e10a5
Yury <66903760+bezlla@users.noreply.github.com>
2021-10-08 06:36:03 +0300

    Рефакторинг & Добавлена возможность получения актуальной конфигурации

--- src/KinectTvV2.API.Core/Services/Admin/AdminService.cs
+++ src/KinectTvV2.API.Core/Services/Admin/AdminService.cs
@@ -2,26 +2,39 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using KinectTvV2.API.Core.Hubs;
+using KinectTvV2.API.Core.Models.ITTV;
 using KinectTvV2.API.Core.Models.S3;
 using KinectTvV2.API.Core.Providers.S3;
+using KinectTvV2.API.Domain.Entities;
+using KinectTvV2.API.Infrastructure.Data;
+using KinectTvV2.Core.Providers.LocalCache;
 
 namespace KinectTvV2.API.Core.Services.Admin
 {
     public class AdminService : IAdminService
     {
+        private const string ITTVConfigurationPath = "ittv-configuration";
+        
         private readonly IS3Provider _s3Provider;
         private readonly KinectTvHub _kinectTvHub;
+        private readonly ApplicationDbContext _dbContext;
         public AdminService(IS3Provider s3Provider,
-            KinectTvHub kinectTvHub)
+            KinectTvHub kinectTvHub, ApplicationDbContext dbContext)
         {
             _s3Provider = s3Provider;
             _kinectTvHub = kinectTvHub;
+            _dbContext = dbContext;
         }
 
+        #region S3
         public async Task UploadFileAsync(Stream fileStream, string fileName, string directoryName = null)
         {
             await _s3Provider.UploadFileAsync(fileStream, fileName, directoryName);
-            //TODO: добавить запись в бд о добавлении файла
+            var fileInfo = new FileInfoEntity(fileName);
+            
+            await _dbContext.AddAsync(fileInfo);
+            await _dbContext.SaveChangesAsync();
+            
             await _kinectTvHub.VideoUploaded(fileName);
         }
         public async Task<S3FileInfo> ReadFileAsync(string fileName, string directoryName = null)
@@ -29,19 +42,29 @@ namespace KinectTvV2.API.Core.Services.Admin
             var file = await _s3Provider.ReadFileAsync(fileName, directoryName);
             return file;
         }
+        #endregion
 
+        #region ITTVConfiguration
         public async Task SetDisplayMessage(string displayMessage)
         {
-            //TODO: обновление дисплей сообщения
+            var configuration = await LocalCacheProvider.GetAsync<ITTVConfiguration>(ITTVConfigurationPath) ?? new ITTVConfiguration();
+            configuration.SetDisplayMessage(displayMessage);
+            await LocalCacheProvider.PutAsync(ITTVConfigurationPath, configuration);
             await _kinectTvHub.SettingsUpdated();
         }
 
         public async Task SetActiveTime(TimeSpan timeFrom, TimeSpan timeTo)
         {
-            //TODO: обновление времени активности
+            var configuration = await LocalCacheProvider.GetAsync<ITTVConfiguration>(ITTVConfigurationPath) ?? new ITTVConfiguration();
+            configuration.SetActiveTime(timeFrom, timeTo);
+            await LocalCacheProvider.PutAsync(ITTVConfigurationPath, configuration);
             await _kinectTvHub.SettingsUpdated();
         }
 
+        public async Task<ITTVConfiguration> GetTvConfiguration()
+        => await LocalCacheProvider.GetAsync<ITTVConfiguration>(ITTVConfigurationPath) ?? new ITTVConfiguration();
+        #endregion
+        
         public async Task Restart()
         {
             await _kinectTvHub.Restart();
