c04138177e537fc82bbdf773ee0c875d8a1cfac7
y.fomenkov <mail.ru>
2021-11-17 14:46:42 +0300

    Зарегистрирован KinectTvHubHandler

--- src/KinectTvV2.API.Core/Hubs/KinectTvHub/IKinectTvHubHandler.cs
+++ src/KinectTvV2.API.Core/Hubs/KinectTvHub/IKinectTvHubHandler.cs
@@ -4,8 +4,8 @@ namespace KinectTvV2.API.Core.Hubs.KinectTvHub
 {
     public interface IKinectTvHubHandler
     {
-        public Task Restart();
-        public Task VideoUploaded(string baseFileName);
-        public Task SettingsUpdated();
+        Task Restart();
+        Task VideoUploaded(string baseFileName);
+        Task SettingsUpdated();
     }
 }
