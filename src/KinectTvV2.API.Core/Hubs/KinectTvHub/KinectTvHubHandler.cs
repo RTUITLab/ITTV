4a3591d886cd857a088c35fd698357ae38ac6dde
y.fomenkov <mail.ru>
2021-11-17 14:41:35 +0300

    Refactoring

---
+++ src/KinectTvV2.API.Core/Hubs/KinectTvHub/KinectTvHubHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace KinectTvV2.API.Core.Hubs.KinectTvHub
+{
+    public class KinectTvHubHandler : IKinectTvHubHandler
+    {
+        private readonly IHubContext<KinectTvHub> _hubContext;
+
+        public KinectTvHubHandler(IHubContext<KinectTvHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task Restart()
+        {
+            await _hubContext.Clients.All.SendAsync(nameof(Restart));
+        }
+        public async Task VideoUploaded(string baseFileName)
+        {
+            await _hubContext.Clients.All.SendAsync(nameof(VideoUploaded), baseFileName);
+        }        
+        public async Task SettingsUpdated()
+        {
+            await _hubContext.Clients.All.SendAsync(nameof(SettingsUpdated));
+        }
+    }
+}
