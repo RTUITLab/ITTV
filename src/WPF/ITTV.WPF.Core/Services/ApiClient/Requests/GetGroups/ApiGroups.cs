f30394695c2bad8a8ab2bc4c1af9e0e3ee7c4227
y.fomenkov <mail.ru>
2022-01-02 20:57:01 +0300

    Refactoring

--- src/WPF/ITTV.WPF.Core/Services/ApiClient/Requests/GetGroups/ApiGroups.cs
+++ src/WPF/ITTV.WPF.Core/Services/ApiClient/Requests/GetGroups/ApiGroups.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 
 namespace ITTV.WPF.Core.Services.ApiClient.Requests.GetGroups
@@ -9,5 +10,12 @@ namespace ITTV.WPF.Core.Services.ApiClient.Requests.GetGroups
 
         [JsonProperty("master")]
         public ApiGroupsMasters Master { get; set; }
+        
+        public DateTime? Updated { get; private set; } = DateTime.Now;
+
+        public void SetUpdated()
+        {
+            Updated = DateTime.Now;
+        }
     }
 }
--- src/WPF/ITTV.WPF/DataModel/Models/Groups.cs
+++ src/WPF/ITTV.WPF/DataModel/Models/Groups.cs
@@ -5,27 +5,65 @@ namespace ITTV.WPF.DataModel.Models
 {
     public class Groups
     {
-        public BachelorGroups bachelor;
-        public MasterGroups master;
+        public Groups()
+        { }
+
+        public Groups(BachelorGroups bachelor, MasterGroups master, DateTime? updated)
+        {
+            Bachelor = bachelor;
+            Master = master;
+            Updated = updated;
+            
+            SetUpdated();
+        }
+        public BachelorGroups Bachelor { get; set; }
+        public MasterGroups Master { get; set; }
 
         public class BachelorGroups
         {
-            public List<Direction> first;
-            public List<Direction> second;
-            public List<Direction> third;
-            public List<Direction> fourth;
+            public BachelorGroups()
+            { }
+
+            public BachelorGroups(List<Direction> first,
+                List<Direction> second,
+                List<Direction> third,
+                List<Direction> fourth)
+            {
+                First = first;
+                Second = second;
+                Third = third;
+                Fourth = fourth;
+            }
+            public List<Direction> First { get; set; }
+            public List<Direction> Second { get; set; }
+            public List<Direction> Third { get; set; }
+            public List<Direction> Fourth { get; set; }
         }
 
         public class MasterGroups
         {
-            public List<Direction> first;
-            public List<Direction> second;
+            public MasterGroups()
+            { }
+            public MasterGroups(List<Direction> first, List<Direction> second)
+            {
+                First = first;
+                Second = second;
+            }
+            public List<Direction> First { get; set; }
+            public List<Direction> Second { get; set; }
         }
 
         public class Direction
         {
-            public string name;
-            public List<string> numbers;
+            public Direction()
+            { }
+            public Direction(string name, string[] numbers)
+            {
+                this.Name = name;
+                this.Numbers = numbers;
+            }
+            public string Name { get; set; }
+            public string[] Numbers { get; set; }
         }
 
         public DateTime? Updated { get; private set; }
