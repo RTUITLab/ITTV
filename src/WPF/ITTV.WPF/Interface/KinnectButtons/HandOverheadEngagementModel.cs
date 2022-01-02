0586e807224199a440c494e896245629344b00c7
y.fomenkov <mail.ru>
2022-01-02 21:06:38 +0300

    Refactoring

--- src/WPF/ITTV.WPF/Interface/KinnectButtons/HandOverheadEngagementModel.cs
+++ src/WPF/ITTV.WPF/Interface/KinnectButtons/HandOverheadEngagementModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Input;
 using Microsoft.Kinect.Toolkit.Input;
@@ -9,30 +10,27 @@ namespace ITTV.WPF.Interface.KinnectButtons
 {
     public class HandOverheadEngagementModel : IKinectEngagementManager
     {
-        bool stopped = true;
-        BodyFrameReader bodyReader;
-        List<Body> bodies;
-        bool engagementPeopleHaveChanged;
-        List<BodyHandPair> handsToEngage;
-        private int engagedPeopleAllowed;
+        private bool _stopped = true;
+        private readonly BodyFrameReader _bodyReader;
+        private readonly List<Body> _bodies;
+        private bool _engagementPeopleHaveChanged;
+        private readonly List<BodyHandPair> _handsToEngage;
+        private int _engagedPeopleAllowed;
 
         public HandOverheadEngagementModel(int engagedPeopleAllowed)
         {
-            this.EngagedPeopleAllowed = engagedPeopleAllowed;
+            EngagedPeopleAllowed = engagedPeopleAllowed;
             var sensor = KinectSensor.GetDefault();
-            this.bodyReader = sensor.BodyFrameSource.OpenReader();
-            this.bodyReader.FrameArrived += this.BodyReader_FrameArrived;
+            _bodyReader = sensor.BodyFrameSource.OpenReader();
+            _bodyReader.FrameArrived += BodyReader_FrameArrived;
             sensor.Open();
-            this.bodies = new List<Body>();
-            this.handsToEngage = new List<BodyHandPair>();
+            _bodies = new List<Body>();
+            _handsToEngage = new List<BodyHandPair>();
         }
 
         public int EngagedPeopleAllowed
         {
-            get
-            {
-                return this.engagedPeopleAllowed;
-            }
+            get => _engagedPeopleAllowed;
             set
             {
                 if (value > 2 || value < 0)
@@ -40,51 +38,45 @@ namespace ITTV.WPF.Interface.KinnectButtons
                     throw new ArgumentOutOfRangeException("value", value, "This engagement manager requires 0 to 2 people to be set as the EngagedPeopleAllowed");
                 }
 
-                this.engagedPeopleAllowed = value;
+                _engagedPeopleAllowed = value;
             }
         }
 
         public bool EngagedBodyHandPairsChanged()
         {
-            return this.engagementPeopleHaveChanged;
+            return _engagementPeopleHaveChanged;
         }
 
-        public IReadOnlyList<BodyHandPair> KinectManualEngagedHands
-        {
-            get
-            {
-                return KinectCoreWindow.KinectManualEngagedHands;
-            }
-        }
+        public IReadOnlyList<BodyHandPair> KinectManualEngagedHands => KinectCoreWindow.KinectManualEngagedHands;
 
         public void StartManaging()
         {
-            this.stopped = false;
-            this.bodyReader.IsPaused = false;
+            _stopped = false;
+            _bodyReader.IsPaused = false;
         }
 
         public void StopManaging()
         {
-            this.stopped = true;
-            this.bodyReader.IsPaused = true;
+            _stopped = true;
+            _bodyReader.IsPaused = true;
         }
 
         private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs args)
         {
-            bool gotData = false;
+            var gotData = false;
 
             using (var frame = args.FrameReference.AcquireFrame())
             {
                 if (frame != null)
                 {
-                    frame.GetAndRefreshBodyData(bodies);
+                    frame.GetAndRefreshBodyData(_bodies);
                     gotData = true;
                 }
             }
 
-            if (gotData && !stopped)
+            if (gotData && !_stopped)
             {
-                this.TrackEngagedPlayersViaHandOverHead();
+                TrackEngagedPlayersViaHandOverHead();
             }
         }
 
@@ -102,88 +94,82 @@ namespace ITTV.WPF.Interface.KinnectButtons
 
         private void TrackEngagedPlayersViaHandOverHead()
         {
-            this.engagementPeopleHaveChanged = false;
+            _engagementPeopleHaveChanged = false;
             var currentlyEngagedHands = KinectCoreWindow.KinectManualEngagedHands;
-            this.handsToEngage.Clear();
+            _handsToEngage.Clear();
 
             // check to see if anybody who is currently engaged should be disengaged
             foreach (var bodyHandPair in currentlyEngagedHands)
             {
                 var bodyTrackingId = bodyHandPair.BodyTrackingId;
-                foreach (var body in this.bodies)
+                foreach (var body in _bodies.Where(x => x.TrackingId == bodyTrackingId))
                 {
-                    if (body.TrackingId == bodyTrackingId)
-                    {
-                        // check for disengagement
-                        JointType engagedHandJoint =
-                            (bodyHandPair.HandType == HandType.LEFT) ? JointType.HandLeft : JointType.HandRight;
-                        bool toBeDisengaged = HandOverheadEngagementModel.IsHandBelowHip(engagedHandJoint, body);
+                    var engagedHandJoint = bodyHandPair.HandType == HandType.LEFT ? JointType.HandLeft : JointType.HandRight;
+                    var toBeDisengaged = IsHandBelowHip(engagedHandJoint, body);
 
-                        if (toBeDisengaged)
-                        {
-                            this.engagementPeopleHaveChanged = true;
-                        }
-                        else
-                        {
-                            this.handsToEngage.Add(bodyHandPair);
-                        }
+                    if (toBeDisengaged)
+                    {
+                        _engagementPeopleHaveChanged = true;
+                    }
+                    else
+                    {
+                        _handsToEngage.Add(bodyHandPair);
                     }
                 }
             }
 
             // check to see if anybody should be engaged, if not already engaged
-            foreach (var body in this.bodies)
+            foreach (var body in _bodies)
             {
-                if (this.handsToEngage.Count < this.engagedPeopleAllowed)
+                if (_handsToEngage.Count >= _engagedPeopleAllowed)
+                    continue;
+                var alreadyEngaged = false;
+                foreach (var bodyHandPair in _handsToEngage)
                 {
-                    bool alreadyEngaged = false;
-                    foreach (var bodyHandPair in this.handsToEngage)
-                    {
-                        alreadyEngaged = (body.TrackingId == bodyHandPair.BodyTrackingId);
-                    }
+                    alreadyEngaged = (body.TrackingId == bodyHandPair.BodyTrackingId);
+                }
 
-                    if (!alreadyEngaged)
+                if (!alreadyEngaged)
+                {
+                    // check for engagement
+                    if (IsHandOverhead(JointType.HandLeft, body))
                     {
-                        // check for engagement
-                        if (HandOverheadEngagementModel.IsHandOverhead(JointType.HandLeft, body))
-                        {
-                            // engage the left hand
-                            this.handsToEngage.Add(
-                                new BodyHandPair(body.TrackingId, HandType.LEFT));
-                            this.engagementPeopleHaveChanged = true;
-                        }
-                        else if (HandOverheadEngagementModel.IsHandOverhead(JointType.HandRight, body))
-                        {
-                            // engage the right hand
-                            this.handsToEngage.Add(
-                                new BodyHandPair(body.TrackingId, HandType.RIGHT));
-                            this.engagementPeopleHaveChanged = true;
-                        }
+                        // engage the left hand
+                        _handsToEngage.Add(
+                            new BodyHandPair(body.TrackingId, HandType.LEFT));
+                        _engagementPeopleHaveChanged = true;
+                    }
+                    else if (IsHandOverhead(JointType.HandRight, body))
+                    {
+                        // engage the right hand
+                        _handsToEngage.Add(
+                            new BodyHandPair(body.TrackingId, HandType.RIGHT));
+                        _engagementPeopleHaveChanged = true;
                     }
                 }
             }
 
-            if (this.engagementPeopleHaveChanged)
+            if (_engagementPeopleHaveChanged)
             {
                 BodyHandPair firstPersonToEngage = null;
                 BodyHandPair secondPersonToEngage = null;
 
-                Debug.Assert(this.handsToEngage.Count <= 2, "handsToEngage should be <= 2");
+                Debug.Assert(_handsToEngage.Count <= 2, "handsToEngage should be <= 2");
 
-                switch (this.handsToEngage.Count)
+                switch (_handsToEngage.Count)
                 {
                     case 0:
                         break;
                     case 1:
-                        firstPersonToEngage = this.handsToEngage[0];
+                        firstPersonToEngage = _handsToEngage[0];
                         break;
                     case 2:
-                        firstPersonToEngage = this.handsToEngage[0];
-                        secondPersonToEngage = this.handsToEngage[1];
+                        firstPersonToEngage = _handsToEngage[0];
+                        secondPersonToEngage = _handsToEngage[1];
                         break;
                 }
 
-                switch (this.EngagedPeopleAllowed)
+                switch (EngagedPeopleAllowed)
                 {
                     case 1:
                         KinectCoreWindow.SetKinectOnePersonManualEngagement(firstPersonToEngage);
