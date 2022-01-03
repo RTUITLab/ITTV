eb409560babeda8e49ae469585b38fa9edd50704
y.fomenkov <mail.ru>
2022-01-03 17:09:55 +0300

    Rf control extracted

--- src/WPF/ITTV.WPF/SettingsService.cs
+++ src/WPF/ITTV.WPF/SettingsService.cs
@@ -3,54 +3,102 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ITTV.WPF.DataModel;
-using ITTV.WPF.Network.Controll;
 using Newtonsoft.Json;
 
 namespace ITTV.WPF
 {
+    //TODO: refactoring, not right using concept 
     public class SettingsService : Singleton<SettingsService>
     {
-        bool configured;
-
         public event Action SettingsUpdated;
-        [JsonProperty]
-        bool? needCheckTime;
-        [JsonProperty]
-        double? sleepHour;
-        [JsonProperty]
-        bool? isAdmin;
-        [JsonProperty]
-        double? videoVolume;
-        [JsonProperty]
-        double? minForUpdate;
-        [JsonProperty]
-        List<string> backgroundVideoOrder;
-
-        private void GetData()
+        public bool NeedCheckTime
         {
-            if (!configured)
+            get
             {
-                ConfigControlLogic.Instance.GetSettingsData = () =>
-                    new List<ConfigControlLogic.StateControlSetting<object>>()
-                    {
-                        new ConfigControlLogic.StateControlSetting<object>("needCheckTime",
-                            ConfigControlLogic.StateControlSetting<object>.DataTypes.Bool, NeedCheckTime),
-                        new ConfigControlLogic.StateControlSetting<object>("sleepHour",
-                            ConfigControlLogic.StateControlSetting<object>.DataTypes.Num, sleepHour),
-                        new ConfigControlLogic.StateControlSetting<object>("isAdmin",
-                            ConfigControlLogic.StateControlSetting<object>.DataTypes.Bool, IsAdmin),
-                        new ConfigControlLogic.StateControlSetting<object>("videoVolume",
-                            ConfigControlLogic.StateControlSetting<object>.DataTypes.Num, videoVolume),
-                        new ConfigControlLogic.StateControlSetting<object>("minForUpdate",
-                            ConfigControlLogic.StateControlSetting<object>.DataTypes.Num, MinForUpdate),
-                        new ConfigControlLogic.StateControlSetting<object>("backgroundVideoOrder",
-                            ConfigControlLogic.StateControlSetting<object>.DataTypes.List, BackgroundVideoOrder),
-                    };
-                ConfigControlLogic.Instance.SettingFilePath = AppDomain.CurrentDomain.BaseDirectory + AllPaths.FileSettingsPath;
-                ConfigControlLogic.Instance.SettingsUpdated += GetData;
-                configured = true;
+                if (Instance.needCheckTime == null)
+                {
+                    UpdateSettings();
+                }
+                return (bool) Instance.needCheckTime;
             }
+        }
 
+        public int SleepHour
+        {
+            get
+            {
+                if (Instance.sleepHour == null)
+                {
+                    UpdateSettings();
+                }
+                return (int)Instance.sleepHour;
+            }
+        }
+
+        public bool IsAdmin
+        {
+            get
+            {
+                if (Instance.isAdmin == null)
+                {
+                    UpdateSettings();
+                }
+                return Instance.isAdmin != null && (bool)Instance.isAdmin;
+            }
+        }
+
+        public double VideoVolume
+        {
+            get
+            {
+                if (Instance.videoVolume == null)
+                {
+                    UpdateSettings();
+                }
+
+                return Math.Min(1, Math.Max(0, Instance.videoVolume.Value) / 100);
+            }
+        }
+
+        public int MinForUpdate
+        {
+            get
+            {
+                if (Instance.minForUpdate == null)
+                {
+                    UpdateSettings();
+                }
+                return (int)Instance.minForUpdate;
+            }
+        }
+
+        public List<string> BackgroundVideoOrder
+        {
+            get
+            {
+                if (Instance.backgroundVideoOrder == null)
+                {
+                    UpdateSettings();
+                }
+                return Instance.backgroundVideoOrder;
+            }
+        }
+        
+        [JsonProperty]
+        private bool? needCheckTime;
+        [JsonProperty]
+        private int? sleepHour;
+        [JsonProperty]
+        private bool? isAdmin;
+        [JsonProperty]
+        private int? videoVolume;
+        [JsonProperty]
+        private int? minForUpdate;
+        [JsonProperty]
+        private List<string> backgroundVideoOrder;
+
+        private void UpdateSettings()
+        {
             if (!File.Exists(AllPaths.FileSettingsPath)) 
                 return;
             
@@ -69,84 +117,10 @@ namespace ITTV.WPF
                     instance.backgroundVideoOrder = CreateVideoData().ToList();
                 }
 
-            ConfigControlLogic.Instance.SendSettingsData();
             SettingsUpdated?.Invoke();
         }
 
         private static IEnumerable<string> CreateVideoData()
             => Directory.GetFiles(AllPaths.GetDirectoryBackgroundVideosPath);
-
-        public bool NeedCheckTime
-        {
-            get
-            {
-                if (Instance.needCheckTime == null)
-                {
-                    GetData();
-                }
-                return (bool) Instance.needCheckTime;
-            }
-        }
-
-        public int SleepHour
-        {
-            get
-            {
-                if (Instance.sleepHour == null)
-                {
-                    GetData();
-                }
-                return (int)Instance.sleepHour;
-            }
-        }
-
-        public bool IsAdmin
-        {
-            get
-            {
-                if (Instance.isAdmin == null)
-                {
-                    GetData();
-                }
-                return (bool)Instance.isAdmin;
-            }
-        }
-
-        public double VideoVolume
-        {
-            get
-            {
-                if (Instance.videoVolume == null)
-                {
-                    GetData();
-                }
-
-                return Math.Min(1, Math.Max(0, Instance.videoVolume.Value) / 100);
-            }
-        }
-
-        public int MinForUpdate
-        {
-            get
-            {
-                if (Instance.minForUpdate == null)
-                {
-                    GetData();
-                }
-                return (int)Instance.minForUpdate;
-            }
-        }
-
-        public List<string> BackgroundVideoOrder
-        {
-            get
-            {
-                if (Instance.backgroundVideoOrder == null)
-                {
-                    GetData();
-                }
-                return Instance.backgroundVideoOrder;
-            }
-        }
     }
 }
