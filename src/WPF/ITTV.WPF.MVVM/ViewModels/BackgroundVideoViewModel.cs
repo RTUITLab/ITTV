637feda8bef1d4120dba1dae34093dec15b321c4
y.fomenkov <y.fomenkov>
2022-01-07 01:20:55 +0300

    Added binding to volume

--- src/WPF/ITTV.WPF.MVVM/ViewModels/BackgroundVideoViewModel.cs
+++ src/WPF/ITTV.WPF.MVVM/ViewModels/BackgroundVideoViewModel.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using ITTV.WPF.MVVM.Models;
 using ITTV.WPF.MVVM.Services;
+using Microsoft.Extensions.Options;
 
 namespace ITTV.WPF.MVVM.ViewModels
 {
     public class BackgroundVideoViewModel : ViewModelBase
     {
         private readonly BackgroundVideoPlaylistService _backgroundVideoPlaylistService;
-        public BackgroundVideoViewModel(BackgroundVideoPlaylistService backgroundVideoPlaylistService)
+        private readonly Settings _settings;
+        public BackgroundVideoViewModel(BackgroundVideoPlaylistService backgroundVideoPlaylistService, IOptions<Settings> settings)
         {
             _backgroundVideoPlaylistService = backgroundVideoPlaylistService;
+            _settings = settings.Value;
             
-            SetNextVideo();
+            Setup();
         }
 
-        private void SetNextVideo()
+        private void Setup()
         {
+            if (!_backgroundVideoPlaylistService.ContainsAnyVideos()) 
+                return;
+            
             CurrentVideo = _backgroundVideoPlaylistService.NextVideo();
+            Volume = _settings.VideoVolume;
         }
         
         private Uri _currentVideo;
@@ -31,6 +39,21 @@ namespace ITTV.WPF.MVVM.ViewModels
                 OnPropertyChanged(nameof(CurrentVideo));
             }
         }
-        
+
+        private double _volume;
+
+        public double Volume
+        {
+            get => _volume;
+            set
+            {
+                if (Equals(_volume, value))
+                    return;
+                
+                _volume = value;
+                OnPropertyChanged(nameof(Volume));
+            }
+        }
+
     }
 }
