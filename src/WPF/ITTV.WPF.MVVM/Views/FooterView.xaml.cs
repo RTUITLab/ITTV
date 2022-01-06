93e6ce8c8b5565329c9b5512d12d2ff5a8e8a324
y.fomenkov <y.fomenkov>
2022-01-06 23:36:50 +0300

    Refactoring

--- src/WPF/ITTV.WPF.MVVM/Views/FooterView.xaml.cs
+++ src/WPF/ITTV.WPF.MVVM/Views/FooterView.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System.Windows.Controls;
-using ITTV.WPF.MVVM.ViewModels;
 
 namespace ITTV.WPF.MVVM.Views
 {
     public partial class FooterView : UserControl
     {
-        public FooterView(FooterViewModel footerViewModel)
-        {
-            InitializeComponent();
-            DataContext = footerViewModel;
-        }
-
         public FooterView()
         {
             InitializeComponent();
