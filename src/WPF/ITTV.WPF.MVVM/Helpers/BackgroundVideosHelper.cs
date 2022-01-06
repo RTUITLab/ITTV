2d0278d90beae8b7d2315d1b9ad75564ad9d7986
y.fomenkov <y.fomenkov>
2022-01-07 01:01:35 +0300

    Refactoring logic with background video

---
+++ src/WPF/ITTV.WPF.MVVM/Helpers/BackgroundVideosHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ITTV.WPF.MVVM.Helpers
+{
+    public static class BackgroundVideosHelper
+    {
+        public static IEnumerable<string> GetBackgroundVideos()
+        {
+            var supportedVideoFormats = new[]
+            {
+                "mov", 
+                "ogg",
+                "mp4"
+            };
+            
+            var fileNames = Directory.GetFiles(AllPaths.GetDirectoryBackgroundVideosPath);
+            
+            var filteredFileNames = fileNames.Where(x =>
+            {
+                var fileExtension = Path.GetExtension(x);
+                
+                var fileSupported = supportedVideoFormats.Contains(fileExtension);
+                if (!fileSupported)
+                {
+                    //TODO: Rewrite logger
+                    //MainWindow.Log($"The format of the background video file {x} is not supported");
+                }
+
+                return fileSupported;
+            });
+
+            return filteredFileNames;
+        }
+    }
+}
--- src/WPF/ITTV.WPF.MVVM/ViewModels/BackgroundVideoViewModel.cs
+++ src/WPF/ITTV.WPF.MVVM/ViewModels/BackgroundVideoViewModel.cs
@@ -1,7 +1,36 @@
-﻿namespace ITTV.WPF.MVVM.ViewModels
+﻿using System;
+using ITTV.WPF.MVVM.Services;
+
+namespace ITTV.WPF.MVVM.ViewModels
 {
     public class BackgroundVideoViewModel : ViewModelBase
     {
+        private readonly BackgroundVideoPlaylistService _backgroundVideoPlaylistService;
+        public BackgroundVideoViewModel(BackgroundVideoPlaylistService backgroundVideoPlaylistService)
+        {
+            _backgroundVideoPlaylistService = backgroundVideoPlaylistService;
+            
+            SetNextVideo();
+        }
+
+        private void SetNextVideo()
+        {
+            CurrentVideo = _backgroundVideoPlaylistService.NextVideo();
+        }
+        
+        private Uri _currentVideo;
+        public Uri CurrentVideo
+        {
+            get => _currentVideo;
+            set
+            {
+                if (Equals(_currentVideo, value))
+                    return;
+
+                _currentVideo = value;
+                OnPropertyChanged(nameof(CurrentVideo));
+            }
+        }
         
     }
 }
