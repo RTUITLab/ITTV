33199ae006b88db9243f81504b9dcea465a43992
y.fomenkov <y.fomenkov>
2022-01-06 23:36:50 +0300

    Refactoring

--- src/WPF/ITTV.WPF.MVVM/Views/BackgroundVideoView.xaml.cs
+++ src/WPF/ITTV.WPF.MVVM/Views/BackgroundVideoView.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System.Windows.Controls;
-using ITTV.WPF.MVVM.ViewModels;
 
 namespace ITTV.WPF.MVVM.Views
 {
     public partial class BackgroundVideoView : UserControl
     {
-        public BackgroundVideoView(BackgroundVideoViewModel backgroundVideoViewModel)
-        {
-            InitializeComponent();
-            DataContext = backgroundVideoViewModel;
-        }
-
         public BackgroundVideoView()
         {
             InitializeComponent();
