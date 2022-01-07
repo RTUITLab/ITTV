fde6091668e768f9df252277d8ed4ee1cd4186a3
y.fomenkov <y.fomenkov>
2022-01-07 16:25:35 +0300

    Added navigation

--- src/WPF/ITTV.WPF.MVVM/Services/NavigationService.cs
+++ src/WPF/ITTV.WPF.MVVM/Services/NavigationService.cs
@@ -7,17 +7,17 @@ namespace ITTV.WPF.MVVM.Services
     public class NavigationService<TViewModel> where TViewModel : ViewModelBase
     {
         private readonly NavigationStore _navigationStore;
-        private readonly Func<TViewModel> _createViewModel;
+        private readonly TViewModel _createdViewModel;
 
-        public NavigationService(NavigationStore navigationStore, Func<TViewModel> createViewModel)
+        public NavigationService(NavigationStore navigationStore, TViewModel createdViewModel)
         {
             _navigationStore = navigationStore;
-            _createViewModel = createViewModel;
+            _createdViewModel = createdViewModel;
         }
 
         public void Navigate()
         {
-            _navigationStore.CurrentViewModel = _createViewModel();
+            _navigationStore.CurrentViewModel = _createdViewModel;
         }
     }
 }
