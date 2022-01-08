2a0d43bdd362a74ca9df73a0d8d6549050a03a24
y.fomenkov <y.fomenkov>
2022-01-08 18:19:18 +0300

    Implement News

--- src/WPF/ITTV.WPF.MVVM/ViewModels/NewsViewModel.cs
+++ src/WPF/ITTV.WPF.MVVM/ViewModels/NewsViewModel.cs
@@ -1,9 +1,28 @@
-﻿using ITTV.WPF.Abstractions.Base.ViewModel;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using ITTV.WPF.Abstractions.Base.ViewModel;
+using ITTV.WPF.Core.Providers.MireaApi;
 
 namespace ITTV.WPF.MVVM.ViewModels
 {
     public class NewsViewModel : ViewModelBase
     {
+        private readonly ObservableCollection<NewsElementViewModel> _news = new();
+        public ObservableCollection<NewsElementViewModel> News => _news;
         
+        private readonly MireaApiProvider _mireaApiProvider;
+        public NewsViewModel(MireaApiProvider mireaApiProvider)
+        {
+            _mireaApiProvider = mireaApiProvider;
+        }
+
+        public async Task SyncNews()
+        {
+            var data = await _mireaApiProvider.GetNews();
+            
+            var newsElements = data.Select(x => 
+                new NewsElementViewModel(x.));
+        }
     }
 }
