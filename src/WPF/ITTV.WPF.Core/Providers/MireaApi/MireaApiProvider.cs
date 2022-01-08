ccfb25c458b79a54e10de1e06558abb26d9a709a
y.fomenkov <y.fomenkov>
2022-01-08 18:12:38 +0300

    Refactoring

---
+++ src/WPF/ITTV.WPF.Core/Providers/MireaApi/MireaApiProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using ITTV.WPF.Core.Helpers;
+using ITTV.WPF.Core.Providers.LocalCache;
+using ITTV.WPF.Core.Services.ApiClient;
+using ITTV.WPF.Core.Services.ApiClient.Requests.GetGroups;
+using ITTV.WPF.Core.Services.ApiClient.Requests.GetNews;
+
+namespace ITTV.WPF.Core.Providers.MireaApi
+{
+    public class MireaApiProvider
+    {
+        private readonly IMireaApiClient _mireaApiClient;
+        
+        public MireaApiProvider(IMireaApiClient mireaApiClient)
+        {
+            _mireaApiClient = mireaApiClient;
+        }
+
+        public async Task<ApiNewsItem[]> GetNews()
+        {
+            var timeUpdated = TimeSpan.FromMinutes(5);
+
+            var dataSource = _mireaApiClient.GetNews();
+
+            var news = await LocalCacheProvider.GetAsync(LocalCacheHelper.NewsCacheKey, dataSource, timeUpdated);
+            return news;
+        }
+        public async Task<ApiGroups> GetGroups()
+        {
+            var timeUpdated = TimeSpan.FromMinutes(5);
+
+            var dataSource = _mireaApiClient.GetAllGroups();
+
+            var groups = await LocalCacheProvider.GetAsync(LocalCacheHelper.GroupsCacheKey, dataSource, timeUpdated);
+            return groups;
+        }
+    }
+}
