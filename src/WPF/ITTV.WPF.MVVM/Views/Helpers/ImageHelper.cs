fc99759edfaec1d06fcb8f103e25969358360642
y.fomenkov <y.fomenkov>
2022-01-10 06:11:10 +0300

    Implemented dark theme

---
+++ src/WPF/ITTV.WPF.MVVM/Views/Helpers/ImageHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ITTV.WPF.MVVM.Views.Helpers
+{
+    public static class ImageHelper
+    {
+        private static void SourceResourceKeyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Image element)
+            {
+                element.SetResourceReference(Image.SourceProperty, e.NewValue);
+            }
+        }
+
+        public static readonly DependencyProperty SourceResourceKeyProperty = DependencyProperty.RegisterAttached(
+            "SourceResourceKey",
+            typeof(object),
+            typeof(ImageHelper),
+            new PropertyMetadata(String.Empty, SourceResourceKeyChanged));
+
+        public static void SetSourceResourceKey(Image element, object value)
+        {
+            element.SetValue(SourceResourceKeyProperty, value);
+        }
+
+        public static object GetSourceResourceKey(Image element)
+        {
+            return element.GetValue(SourceResourceKeyProperty);
+        }
+    }
+}
