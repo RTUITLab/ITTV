7605eb733d7c9c6eda21db9c45cdd457bd221f9b
y.fomenkov <y.fomenkov>
2022-01-10 06:53:37 +0300

    Implemented inactive mode

--- src/WPF/ITTV.WPF.Core/Services/UserInterfaceManager.cs
+++ src/WPF/ITTV.WPF.Core/Services/UserInterfaceManager.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Timers;
+using System.Windows.Input;
+using ITTV.WPF.Core.Models;
+using Microsoft.Extensions.Options;
 
 namespace ITTV.WPF.Core.Services
 {
-    public class UserInterfaceManager
+    public class UserInterfaceManager : IDisposable
     {
+        private readonly Timer _timer;
+        private readonly Settings _settings;
+        private DateTime _lastActivity = DateTime.Now;
+        public event Action OnStateChangedToInactive;
+
+        public UserInterfaceManager(IOptions<Settings> settings)
+        {
+            _settings = settings.Value;
+            
+            InputManager.Current.PreProcessInput += CurrentOnPreProcessInput;
+            
+            _timer = new Timer
+            {
+                Interval = TimeSpan.FromSeconds(1).Seconds * 1000,
+                Enabled = true
+            };
+            _timer.Elapsed += TimerOnElapsed;
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (DateTime.Now - _lastActivity > _settings.InactiveModeTime)
+            {
+                OnStateChangedToInactive?.Invoke();
+            }
+        }
+
+        private void CurrentOnPreProcessInput(object sender, PreProcessInputEventArgs e)
+        {
+            _lastActivity = DateTime.Now;
+        }
+
         public bool IsDarkTheme { get; private set; } = true;
 
         public event Action ThemeUpdated;
@@ -17,6 +53,11 @@ namespace ITTV.WPF.Core.Services
         protected virtual void OnThemeUpdated()
         {
             ThemeUpdated?.Invoke();
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
         }
     }
 }
