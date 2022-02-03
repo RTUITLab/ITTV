a23bd5bf825da139c5dcf47a7b24d65f0adafadb
y.fomenkov <y.fomenkov>
2022-02-03 09:01:35 +0300

    Refactoring && Added field for tracking loaded view status

--- src/WPF/ITTV.WPF.Abstractions/Base/ViewModel/ViewModelBase.cs
+++ src/WPF/ITTV.WPF.Abstractions/Base/ViewModel/ViewModelBase.cs
@@ -4,6 +4,24 @@ namespace ITTV.WPF.Abstractions.Base.ViewModel
 {
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public bool IsLoaded
+        {
+            get => _isLoaded;
+            set
+            {
+                if (Equals(_isLoaded, value))
+                    return;
+                _isLoaded = value;
+                OnPropertyChanged(nameof(IsLoaded));
+            }
+        }
+        private bool _isLoaded;
+
+        protected void SetLoaded()
+        {
+            IsLoaded = true;
+        }
+        
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName)
