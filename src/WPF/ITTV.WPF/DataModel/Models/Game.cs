d9ff0e2e55f8000136913527ca4dbb8eebad3e59
y.fomenkov <y.fomenkov>
2022-02-15 06:19:40 +0300

    Extracted old version

--- src/WPF/ITTV.WPF/DataModel/Models/Game.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ITTV.WPF.DataModel.Models
-{
-    public class Game : DataExecuteBase
-    {
-        public Game(string uniqueId, string title, string[] param) : base(uniqueId, title, param) { }
-    }
-}
