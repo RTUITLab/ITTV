85bfe15757823d16223afb27cbf206cf6734c338
y.fomenkov <y.fomenkov>
2022-02-17 12:52:46 +0300

    Added notification to admin mode

--- src/WPF/ITTV.WPF.MVVM/ViewModels/NotificationViewModel.cs
+++ src/WPF/ITTV.WPF.MVVM/ViewModels/NotificationViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using ITTV.WPF.Abstractions.Base.ViewModel;
+using ITTV.WPF.Core.Models;
 using ITTV.WPF.Core.Stores;
+using Microsoft.Extensions.Options;
 
 namespace ITTV.WPF.MVVM.ViewModels
 {
     public class NotificationViewModel : ViewModelBase
     {
         public Notification ActiveNotification => _notificationStore.ActiveNotification;
-        public bool HasActiveNotification => ActiveNotification != null;
+        public bool HasActiveNotification => _settings.IsAdminMode && ActiveNotification != null;
         
         private readonly NotificationStore _notificationStore;
+        private readonly Settings _settings;
 
-        public NotificationViewModel(NotificationStore notificationStore)
+        public NotificationViewModel(NotificationStore notificationStore,
+            IOptions<Settings> settings)
         {
             _notificationStore = notificationStore;
+            _settings = settings.Value;
 
             _notificationStore.NotificationUpdated += OnNotificationUpdated;
         }
