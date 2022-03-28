d3b0d215121cf16db45c9835e7d954e9fe576ad0
y.fomenkov <y.fomenkov>
2022-03-28 21:15:03 +0300

    Using CacheUpdateInterval for tracking expire of cache

--- src/WPF/ITTV.WPF.Core/Providers/MireaApi/MireaApiProvider.cs
+++ src/WPF/ITTV.WPF.Core/Providers/MireaApi/MireaApiProvider.cs
@@ -1,27 +1,31 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ITTV.WPF.Core.Helpers;
+using ITTV.WPF.Core.Models;
 using ITTV.WPF.Core.Providers.LocalCache;
 using ITTV.WPF.Core.Services.ApiClient;
 using ITTV.WPF.Core.Services.ApiClient.Requests.GetGroups;
 using ITTV.WPF.Core.Services.ApiClient.Requests.GetNews;
 using ITTV.WPF.Core.Services.ApiClient.Requests.GetScheduleForGroup;
+using Microsoft.Extensions.Options;
 
 namespace ITTV.WPF.Core.Providers.MireaApi
 {
     public class MireaApiProvider
     {
-        private readonly TimeSpan _timeForUpdate = TimeSpan.FromHours(1);
         private readonly IMireaApiClient _mireaApiClient;
+        private readonly Settings _settings;
         
-        public MireaApiProvider(IMireaApiClient mireaApiClient)
+        public MireaApiProvider(IMireaApiClient mireaApiClient,
+                IOptions<Settings> settings)
         {
             _mireaApiClient = mireaApiClient;
+            _settings = settings.Value;
         }
 
         public async Task<ApiNewsItem[]> GetNews(TimeSpan? expireDateTime = default)
         {
-            var timeUpdated = expireDateTime ?? _timeForUpdate;
+            var timeUpdated = expireDateTime ?? _settings.CacheUpdateInterval;
             
             Func<Task<ApiNewsItem[]>> dataSource = () => _mireaApiClient.GetNews();
 
@@ -30,7 +34,7 @@ namespace ITTV.WPF.Core.Providers.MireaApi
         }
         public async Task<ApiGroups> GetGroups()
         {
-            var timeUpdated = _timeForUpdate;
+            var timeUpdated = _settings.CacheUpdateInterval;
 
             Func<Task<ApiGroups>> dataSource = () => _mireaApiClient.GetAllGroups();
             
@@ -39,7 +43,7 @@ namespace ITTV.WPF.Core.Providers.MireaApi
         }
         public async Task<ApiFullScheduleResponse> GetFullSchedule(string group)
         {
-            var timeUpdated = _timeForUpdate;
+            var timeUpdated = _settings.CacheUpdateInterval;
             
             Func<Task<ApiFullScheduleResponse>> dataSource = () => _mireaApiClient.GetFullScheduleForGroup(group);
             
